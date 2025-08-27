13f60808dcae9381af938fb1fd0c501d39d93ac7
aiquant2 <gauravraj@aiquantgroup.com>
2025-08-27 11:15:47 +0530

    fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,6 @@ plugins {
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
-
-
 }
 
 
